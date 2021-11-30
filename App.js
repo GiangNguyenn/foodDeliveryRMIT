deb2eeecf924468971e95a7578871fb8baaaf24a
Minh <vokhaiminh0811@gmail.com>
2021-11-30 20:21:14 +0700

    Add Tabs navigation

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
 import React from 'react'
-import { AsyncStorage } from 'react-native'
+import { AsyncStorage } from '@react-native-async-storage/async-storage'
 import * as firebase from 'firebase'
 import { createStackNavigator } from '@react-navigation/stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { NavigationContainer } from '@react-navigation/native'
-import Home from 'Home.js'
+import { Home } from './Home'
 import Login from './components/auth/Login'
 import LandingScreen from './components/auth/Landing'
 import { SignUp } from './components/auth/Signup'
 import { RestaurantListing } from './components/restaurant/RestaurantListing'
 import { RestaurantDetail } from './components/restaurant/restaurant-detail/RestaurantDetail'
+import { UserProfile } from './components/profile/UserProfile'
+import { SearchEngine } from './components/search/SearchEngine'
 import { LogBox } from 'react-native'
 
 LogBox.ignoreLogs(['Setting a timer'])
+
 const firebaseConfig = {
     apiKey: 'AIzaSyCA-73uydGV9cFM2ha4ngUuWHNmp-byeFE',
     authDomain: 'rmit-canteen.firebaseapp.com',
@@ -32,39 +36,118 @@ const getCache = async (key) => {
         console.log('caught error', e)
     }
 }
-const Stack = createStackNavigator()
-export default function App() {
-    // const user = getCache('user')
-    const user = ''
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName={user ? 'home' : 'landing'}>
-                <Stack.Screen
-                    name="Landing"
-                    component={LandingScreen}
+
+const AppStack = createStackNavigator()
+const RootStack = createStackNavigator()
+const AuthStack = createStackNavigator()
+const RestaurantStack = createStackNavigator()
+const Tab = createBottomTabNavigator()
+
+const AuthStackScreen = () => (
+    <AuthStack.Navigator initialRouteName={'Landing'}>
+        <AuthStack.Screen
+            name="Landing"
+            component={LandingScreen}
+            options={{ headerShown: false }}
+        ></AuthStack.Screen>
+        <AuthStack.Screen
+            name="login"
+            component={Login}
+            options={{ headerShown: false }}
+        ></AuthStack.Screen>
+        <AuthStack.Screen
+            name="signup"
+            component={SignUp}
+            options={{ headerShown: false }}
+        ></AuthStack.Screen>
+    </AuthStack.Navigator>
+)
+
+const RestaurantStackScreen = () => (
+    <RestaurantStack.Navigator initialRouteName={'restaurant-listing'}>
+        <RestaurantStack.Screen
+            name="restaurant-listing"
+            component={RestaurantListing}
+            options={{ headerShown: false }}
+        ></RestaurantStack.Screen>
+        <RestaurantStack.Screen
+            name="restaurant-detail"
+            component={RestaurantDetail}
+            options={{ headerShown: false }}
+        ></RestaurantStack.Screen>
+    </RestaurantStack.Navigator>
+)
+
+const RootTabs = () => (
+    <Tab.Navigator initialRouteName={'restaurant-stack'}>
+        <Tab.Screen name="restaurant-stack" component={RestaurantStackScreen} />
+        <Tab.Screen name="profile" component={UserProfile} />
+        <Tab.Screen name="search" component={SearchEngine} />
+    </Tab.Navigator>
+)
+
+const AppStackScreen = () => (
+    <AppStack.Navigator initialRouteName={'root-tabs'}>
+        <AppStack.Screen
+            name="root-tabs"
+            component={RootTabs}
+            options={{ headerShown: false }}
+        ></AppStack.Screen>
+    </AppStack.Navigator>
+)
+
+export class App extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+            loaded: false,
+        }
+    }
+
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged((user) => {
+            if (!user) {
+                this.setState({
+                    loggedIn: false,
+                    loaded: true,
+                })
+            } else {
+                this.setState({
+                    loggedIn: true,
+                    loaded: true,
+                })
+            }
+        })
+    }
+
+    render() {
+        const { loggedIn, loaded } = this.state
+
+        !loaded ? (
+            <View style={{ flex: 1, justifyContent: 'center' }}>
+                <Text>Loading</Text>
+            </View>
+        ) : (
+            {}
+        )
+        loggedIn ? (
+            <NavigationContainer>
+                <RootStack.Navigator>
+                    <RootStack.Screen
+                        name="App"
+                        component={AppStackScreen}
+                        options={{ headerShown: false }}
+                    ></RootStack.Screen>
+                </RootStack.Navigator>
+            </NavigationContainer>
+        ) : (
+            <NavigationContainer>
+                <RootStack.Screen
+                    name="Auth"
+                    component={AuthStackScreen}
                     options={{ headerShown: false }}
-                ></Stack.Screen>
-                <Stack.Screen
-                    name="login"
-                    component={Login}
-                    options={{ headerShown: false }}
-                ></Stack.Screen>
-                <Stack.Screen
-                    name="signup"
-                    component={SignUp}
-                    options={{ headerShown: false }}
-                ></Stack.Screen>
-                <Stack.Screen
-                    name="home"
-                    component={Home}
-                    options={{ headerShown: false }}
-                ></Stack.Screen>
-                <Stack.Screen
-                    name="restaurant-detail"
-                    component={RestaurantDetail}
-                    options={{ headerShown: false }}
-                ></Stack.Screen>
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
+                ></RootStack.Screen>{' '}
+            </NavigationContainer>
+        )
+    }
 }
--- Home.js
+++ Home.js
@@ -3,13 +3,11 @@ import { SafeAreaView } from 'react-native'
 import RestaurantListing from './components/restaurant/RestaurantListing'
 import BottomTabs from './components/navigation/BottomTabs'
 
-export default class Home extends Component {
-    render() {
-        return (
-            <SafeAreaView>
-                <RestaurantListing />
-                <BottomTabs />
-            </SafeAreaView>
-        )
-    }
+export default function Home(props) {
+    return (
+        <SafeAreaView>
+            <RestaurantListing />
+            <BottomTabs />
+        </SafeAreaView>
+    )
 }
