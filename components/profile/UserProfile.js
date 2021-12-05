dbaddf36ab07ca884ff23ea65d1f9f228223bbe4
Minh <vokhaiminh0811@gmail.com>
2021-12-05 17:55:58 +0700

    User profile initiation

--- components/profile/UserProfile.js
+++ components/profile/UserProfile.js
@@ -1,5 +1,18 @@
-import React, { Component } from 'react'
-import { Text } from 'react-native'
+import React from 'react';
+import {View, SafeAreaView, StyleSheet} from 'react-native';
+import {
+  Avatar,
+  Title,
+  Caption,
+  Text,
+  TouchableRipple,
+} from 'react-native-paper';
+
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import Share from 'react-native-share';
+
+
 export class UserProfile extends Component {
     render() {
         return (
