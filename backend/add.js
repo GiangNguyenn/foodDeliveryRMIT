38a6f40e46b018f430acbccc67027ec93318f047
bee.vo <vogiabao292@gmail.com>
2021-12-18 07:04:36 +0700

    add parent document ref to add function

--- backend/add.js
+++ backend/add.js
@@ -5,4 +5,12 @@ export const addToCollection = (collection, properties) => {
         .collection(collection)
         .add(properties)
         .catch((err) => console.log(err))
+        .then((docRef) => {
+            console.log('docreffff ', docRef.id)
+            firebase
+                .firestore()
+                .collection(collection)
+                .doc(docRef.id)
+                .update({ ref: docRef.id })
+        })
 }
